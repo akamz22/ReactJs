9b2afe365f3193f5fdecce78ebd5c9cb609c33df
Ashwini <akamzing412@gmail.com>
2023-02-26 22:15:48 +0530

    notes deleted from appjs

--- App.js
+++ App.js
@@ -1,60 +1,5 @@
-/*
----------Features Of Parcel-------------
-*Bundling
-*minify
-*https on dev
-*port number
-*image optimization
-*compression
-*caching while development
-*zero config
-*compatable with older versions
-*consistant hashing algorithm
-*super fast build algorithm
-*cleaning our code 
-*file matcher algorithm c++
-*hot module replacement
-*
-*
-*Transitive dependencies
-*/
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-
-//React.createRoot gives us an Object
-
-/*
-      <div class="header">
-      <h1>Namaste react</h1>
-        <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-        <li>Support</li>
-        </ul>
-      </div>
-*/
-// const heading1 = React.createElement("h1",
-//   { id: "title1", key: "h1" }, "Heading1 from parcel!");
-// const heading2 = React.createElement("h2",
-//   { id: "title2", key: "h2" }, "Heading2!  ");
-// // console.log(heading);
-// console.log(heading1);
-// const container = React.createElement("div", { id: "container", hello: "World" },
-//   [React.createElement("h1", { title: 'h1', key: 'h1' }, "Heading1")
-//     , React.createElement("ul", {}, [React.createElement("li", {},
-//       "Home"
-//     ), React.createElement("li", {},
-//       "About Us"
-//     ), React.createElement("li", {},
-//       "Contact"
-//     ), React.createElement("li", {},
-//       "Support"
-//     )])]
-// );
 //-------------------------JSX----------------------------
 const Title = () => (
   <h1 id="title" key="h2">
@@ -64,17 +9,10 @@ const Title = () => (
 
 
 //----------------React Component-----------------------
-//Functional Componenet & Class based component
-
-
-//Functional Components (just functionn)
-//Name starts with capital letter
-//
-
-// const data = api.getData();
 const HeaderComponent = () => {
   return (
     <div>
+      <Title/>
       <div className="header" id="header">
        <img alt="logo" id="logo" src="./assets/logo.svg"/>
        <img alt="searchBar" id="search" src="./assets/search.png"/>
